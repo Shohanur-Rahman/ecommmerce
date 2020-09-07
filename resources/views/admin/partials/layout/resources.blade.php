37a5e1541e04d05ae609fe28413497367a371f2f
cordialsoftsolutionsdev2 <dev2@cordialsoft.com>
2020-09-07 16:46:18 +0600

    update

---
+++ resources/views/admin/partials/layout/resources.blade.php
@@ -0,0 +1,25 @@
+<meta charset="UTF-8">
+<meta name="description" content="">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+<!-- Title -->
+<title>@yield('title') | Ecommerce admin</title>
+
+<!-- Favicon -->
+<link rel="icon" href="{{asset('admin/img/core-img/favicon.png')}}">
+
+<!-- Master Stylesheet [If you remove this CSS file, your file will be broken undoubtedly.] -->
+<link rel="stylesheet" href="{{asset('admin/style.css')}}">
+<link rel="stylesheet" href="{{asset('admin/custom.css')}}">
+<!-- Must needed plugins to the run this Template -->
+<script src="{{asset('admin/js/jquery.min.js')}}"></script>
+<script src="{{asset('admin/js/popper.min.js')}}"></script>
+<script src="{{asset('admin/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('admin/js/bundle.js')}}"></script>
+<script src="{{asset('admin/js/default-assets/setting.js')}}"></script>
+<script src="{{asset('admin/js/default-assets/fullscreen.js')}}"></script>
+
+<!-- Active JS -->
+<script src="{{asset('admin/js/default-assets/active.js')}}"></script>
