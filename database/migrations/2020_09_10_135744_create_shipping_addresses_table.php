7dc0207f656e1b881567e10dcf6ee7a8fa709b23
Mosharaf20 <mosharafhosen90@gmail.com>
2020-09-10 20:18:34 +0600

    add

---
+++ database/migrations/2020_09_10_135744_create_shipping_addresses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShippingAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shipping_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedMediumInteger('user_id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('house')->nullable();
+            $table->string('road');
+            $table->integer('postcode');
+            $table->string('state');
+            $table->string('city');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shipping_addresses');
+    }
+}
---
+++ database/migrations/2020_09_10_140931_create_user_profiles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedMediumInteger('user_id');
+            $table->string('avatar');
+            $table->string('secondary_email')->unique();
+            $table->string('phone');
+            $table->string('house')->nullable();
+            $table->string('road');
+            $table->integer('postcode');
+            $table->integer('state');
+            $table->string('city');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+}
