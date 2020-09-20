f4e991f9abae114a148f0fae64ada581e9ace965
Mosharaf20 <mosharafhosen90@gmail.com>
2020-09-20 12:41:17 +0600

    update

--- resources/views/user/partials/widget/product_reviews.blade.php
+++ resources/views/user/partials/widget/product_reviews.blade.php
@@ -1,68 +1,68 @@
-<div id="reviews" class="tab-pane fade active show">
-    <div class="blog-comments product-comments mt-0">
-        <ul class="list-none">
-            <li>
 
-                <div class="comment-avatar text-center">
-                    <img src="assets/images/blog/comment/4.jpg" alt="">
-                    <div class="product-rating mt-10">
+    <div id="reviews" class="row fade active show ">
+        <div class="blog-comments product-comments mt-0">
+            <ul class="list-none">
+                <li>
+
+                    <div class="comment-avatar text-center">
+                        <img src="assets/images/blog/comment/4.jpg" alt="">
+                        <div class="product-rating mt-10">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star-o"></i>
+                        </div>
+                    </div>
+                    <div class="comment-desc">
+                        <span>27 Aug 2019</span>
+                        <h4>Jenney Kelley</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                    </div>
+                </li>
+                <li>
+                    <div class="comment-avatar text-center">
+                        <img src="assets/images/blog/comment/5.jpg" alt="">
+                        <div class="product-rating mt-10">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star-o"></i>
+                        </div>
+                    </div>
+                    <div class="comment-desc">
+                        <span>27 Aug 2019</span>
+                        <h4>Brandon William</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="blog-comment-form product-comment-form mt-05">
+            <h4><span>Add Review</span></h4>
+            <div class="row mt-30">
+                <div class="col-sm-6 single-form">
+                    <input type="text" placeholder="Name">
+                </div>
+                <div class="col-sm-6">
+                    <input type="text" placeholder="Email">
+                </div>
+                <div class="col-sm-12">
+                    <div class="product-rating style-2">
+                        <span>Your Rating:</span>
                         <i class="fa fa-star"></i>
                         <i class="fa fa-star"></i>
                         <i class="fa fa-star"></i>
                         <i class="fa fa-star"></i>
-                        <i class="fa fa-star-o"></i>
+                        <i class="fa fa-star"></i>
                     </div>
                 </div>
-                <div class="comment-desc">
-                    <span>27 Aug 2019</span>
-                    <h4>Jenney Kelley</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                <div class="col-sm-12">
+                    <textarea placeholder="Messages"></textarea>
                 </div>
-            </li>
-            <li>
-                <div class="comment-avatar text-center">
-                    <img src="assets/images/blog/comment/5.jpg" alt="">
-                    <div class="product-rating mt-10">
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star-o"></i>
-                    </div>
+                <div class="col-sm-12">
+                    <button class="btn-common mt-25">Submit</button>
                 </div>
-                <div class="comment-desc">
-                    <span>27 Aug 2019</span>
-                    <h4>Brandon William</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-                </div>
-            </li>
-        </ul>
-    </div>
-    <div class="blog-comment-form product-comment-form mt-05">
-        <h4><span>Add Review</span></h4>
-        <div class="row mt-30">
-            <div class="col-sm-6 single-form">
-                <input type="text" placeholder="Name">
-            </div>
-            <div class="col-sm-6">
-                <input type="text" placeholder="Email">
-            </div>
-            <div class="col-sm-12">
-                <div class="product-rating style-2">
-                    <span>Your Rating:</span>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                </div>
-            </div>
-            <div class="col-sm-12">
-                <textarea placeholder="Messages"></textarea>
-            </div>
-            <div class="col-sm-12">
-                <button class="btn-common mt-25">Submit</button>
             </div>
         </div>
-    </div>
-</div>
