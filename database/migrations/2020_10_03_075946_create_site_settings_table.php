f682a1673edfb3392db0be50495dde111f6cfec6
Mosharaf20 <mosharafhosen90@gmail.com>
2020-10-03 14:01:26 +0600

    update

--- database/migrations/2020_10_03_075946_create_site_settings_table.php
+++ database/migrations/2020_10_03_075946_create_site_settings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEcomSupportsTable extends Migration
+class CreateSiteSettingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreateEcomSupportsTable extends Migration
      */
     public function up()
     {
-        Schema::create('ecom_supports', function (Blueprint $table) {
+        Schema::create('site_settings', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('image_url');
             $table->text('description');
-            $table->boolean('is_published')->default(0);
             $table->timestamps();
         });
     }
@@ -30,6 +32,6 @@ class CreateEcomSupportsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('ecom_supports');
+        Schema::dropIfExists('site_settings');
     }
 }
