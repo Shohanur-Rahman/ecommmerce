c69ef083a548f9fbceff02166d6a3cfe8c451720
Shohanur Rahman <shohanur.rahman57@gmail.com>
2020-10-06 16:17:56 +0600

    abacusIT/aboutUs.html

---
+++ public/abacusIT/js/main.js
@@ -0,0 +1,70 @@
+(function ($) {
+    "use strict"
+    var $win = $(window);
+    var $top = $(".back-to-top p ")
+  
+    /*== top to back====*/
+    $win.on('scroll', function () {
+        var $scrolling = $(this).scrollTop();
+
+    });
+    $top.on('click', function () {
+        $("html,body").animate({
+            scrollTop: 0
+        }, 2000);
+    });
+
+  //mileston-reviews-slider
+   $(".mileston-reviews-slider").slick({
+      infinite: true,
+      slidesToShow:1,
+      slidesToScroll:1,
+      autoplay:true,
+      autoplaySpeed:100000,
+      dots:false,
+      fade:false,
+      arrows:true,
+      nextArrow: '<i class="fas fa-chevron-right"></i>',
+      prevArrow: '<i class="fas fa-chevron-left"></i>',
+    });
+    //banner-slider
+   $(".banner-slider").slick({
+      infinite: true,
+      slidesToShow:1,
+      slidesToScroll:1,
+      autoplay:true,
+      autoplaySpeed:3000,
+      dots:true,
+      fade:true,
+      arrows:false
+    });
+     //slide-text
+   $(".slide-text-content").slick({
+      infinite: true,
+      slidesToShow:1,
+      slidesToScroll:1,
+      autoplay:true,
+      autoplaySpeed:4000,
+      dots:false,
+      fade:true,
+      arrows:false
+    });
+   $('.counter').counterUp({
+      delay: 10,
+      time: 800
+  });
+
+  $('.venobox').venobox({
+    framewidth : '600px',                            
+    frameheight: '400px',                            
+    border     : '5px',                             
+    bgcolor : '#fff',                          
+    share      : ['facebook', 'twitter', 'download'],
+    spinner :'double-bounce'
+
+  });
+})(jQuery);
+
+$(document).ready(function() {
+ new WOW().init();
+});
