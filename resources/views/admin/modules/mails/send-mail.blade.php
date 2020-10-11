07c58e545c4988a3e459ddd5f8c40826ebdf0409
Mosharaf20 <mosharafhosen90@gmail.com>
2020-10-11 10:06:34 +0600

    update

--- resources/views/admin/modules/mails/send-mail.blade.php
+++ resources/views/admin/modules/mails/send-mail.blade.php
@@ -49,4 +49,10 @@
                 </div>
             </form>
         </div>
+
+        <script>
+            $('#dummy_delete_btn').click(function () {
+                $('#dummy_mail_form_submit').submit()
+            })
+        </script>
 @endsection
